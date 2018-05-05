10b8b7b7f777641b90f18a4438f20a35c12138be
rberec <berec@umich.edu>
2018-05-04 22:13:04 -0400

    (1) refactored monte carlo pricing function for more general pricing (2) added up and out option

--- FSharpQuantFinanceExperiments/Program.fs
+++ FSharpQuantFinanceExperiments/Program.fs
@@ -10,20 +10,33 @@ open MathNet.Numerics.Random
 let callPayoff strike price = 
     max(price - strike) 0.0
 
+
 let europeanPayoff payoff assetPath =
     assetPath |> Seq.last |> payoff
 
+
 let europeanCallPayoff strike assetPath =
     assetPath |> europeanPayoff (callPayoff strike)
+
+
+let upOutPayoff payoff barier assetPath = 
+    if assetPath |> Seq.exists (fun s -> s >= barier) then 0.0 else payoff assetPath
+
+
+let upOutEuropeanCallPayoff strike barier =
+    upOutPayoff (europeanCallPayoff strike) barier 
+
     
 let getAssetPath s0 r dt sigma (normal:Normal) nSteps =
     Seq.unfold (fun s -> let sNew = (s * exp(((r - (0.5 * sigma * sigma)) * dt) + (sigma * sqrt(dt) * normal.Sample())))
                          Some(s, sNew)) s0
     |> Seq.take (nSteps + 1)
 
+
 let phi x = 
     let normal = new Normal()
     normal.CumulativeDistribution(x)
+
 
 let priceEuropeanCallAnalytic s0 strike r T sigma =
     let discountedK = strike * exp(-r * T)
@@ -33,29 +46,43 @@ let priceEuropeanCallAnalytic s0 strike r T sigma =
     let d_minus = d_ - (0.5 * totalVolatility)
     (s0 * phi(d_plus)) - (discountedK * phi(d_minus))
 
+
 // Monte Carlo pricing
 
-let priceEuropeanCallMC s0 strike r T sigma (seed:int) nSteps nPaths   =
+let priceOptionMC option s0 r T sigma (seed:int) nSteps nPaths =
     let randomSource = new MersenneTwister(seed)
     let normal = new Normal(0.0, 1.0, randomSource)
     let dt = T / float nSteps
-    let payoffs = seq { for n in 1 .. nPaths do 
-                        let assetPath = getAssetPath s0 r dt sigma normal nSteps |> Seq.toList
-                        yield assetPath |> europeanCallPayoff strike
-                      }
+    let payoffs = seq<double> { for n in 1 .. nPaths do 
+                                let assetPath = getAssetPath s0 r dt sigma normal nSteps |> Seq.toList
+                                yield assetPath |> option
+                              }
 
     let df = exp(-r * T)
     let priceMC = df * payoffs.Mean()
     let stddevMC = df * payoffs.StandardDeviation() / sqrt(float nPaths)
     (priceMC, stddevMC)
 
-let europeanCallMC s0 strike r T sigma (seed:int) nSteps nPaths   =    
-    let result = priceEuropeanCallMC s0 strike r T sigma seed nSteps nPaths
-    printfn "European Call  mean:%f stddev:%f (using MC, n = %i)" (fst result) (snd result) (nPaths)
+
+let priceEuropeanCallMC s0 strike r T sigma (seed:int) nSteps nPaths =
+    let option = europeanCallPayoff strike
+    priceOptionMC option s0 r T sigma seed nSteps nPaths
+
+
+let priceUpOutEuropeanCallMC s0 strike barier r T sigma (seed:int) nSteps nPaths = 
+    let option = upOutEuropeanCallPayoff strike barier
+    priceOptionMC option s0 r T sigma seed nSteps nPaths
+  
+
+let runAndPrintResultMC pricer nPaths =
+    let result = pricer nPaths
+    printfn "Option price  mean:%f stddev:%f (using MC, n = %i)" (fst result) (snd result) (nPaths)
+
 
 let europeanCallAnalytic s0 strike r T sigma =
     let result = priceEuropeanCallAnalytic s0 strike r T sigma
     printfn "European Call (analytic) %f" result
+
 
 [<EntryPoint>]
 let main argv = 
@@ -68,7 +95,13 @@ let main argv =
     let nSteps = 12
     let seed = 9318669
 
-    
-    nPaths |> Array.map (europeanCallMC s0 strike r T sigma seed nSteps) |> ignore
+    printfn "Test of European Call Option:"
+    let europeanCallTest = priceEuropeanCallMC s0 strike r T sigma seed nSteps
+    nPaths |> Array.map (runAndPrintResultMC europeanCallTest) |> ignore
     europeanCallAnalytic s0 strike r T sigma
+
+    printfn "Test of European Up and Out Call Option:"
+    let barier = 120.0
+    let europeanUpOutCallTest = priceUpOutEuropeanCallMC s0 strike barier r T sigma seed nSteps
+    nPaths |> Array.map (runAndPrintResultMC europeanUpOutCallTest) |> ignore
     0 // return an integer exit code
